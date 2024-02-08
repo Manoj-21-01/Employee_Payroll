4f5cc3fcc5b50dc6dea768edc6c43d5702275ec7
Manoj Veluru <manojveluru21@gmail.com>
2024-02-08 16:14:25 +0530

    [MANOJ] Added: Added dom features

--- scripts/dom.js
+++ scripts/dom.js
@@ -3,52 +3,43 @@
 function register_employee() {
     let name = document.getElementById("name").value;
     let notes = document.getElementById("notes").value;
-
-    console.log("Name: ",name);
-    console.log("Notes: ",notes);
+    let date = document.getElementById("date").value;
+    let gender = document.getElementById("male").checked;
+    let salary = document.getElementById("salary").value;
+    
+    let dept1 = document.getElementById("dept1").checked;
+    let dept2 = document.getElementById("dept2").checked;
+    let dept3 = document.getElementById("dept3").checked;
+    let dept4 = document.getElementById("dept4").checked;
+    let dept5 = document.getElementById("dept5").checked;
     
     const obj = {
-        Name: "name",
-        Notes: "notes"
+        Name: name,
+        Notes: notes,
+        Gender: gender ? "male" : "female",
+        Department: [],
+        Salary: salary,
+        Start_Date: date
     }
+    let dept_values = []
+    if (dept1){
+        dept_values.push("HR")
+    }
+    if (dept2){
+        dept_values.push("Sales")
+    }
+    if (dept3){
+        dept_values.push("Finance")
+    }
+    if (dept4){
+        dept_values.push("Engineer")
+    }
+    if (dept5){
+        dept_values.push("Others")
+    }
+    obj.Department = dept_values;
+    console.log(obj)
 }
 document.getElementById("submit").addEventListener("click",function(){
     register_employee();
-});
-
-let radioButtons = document.querySelectorAll("input[name='gender']");
-let findSelected = () => {
-    let selected = document.querySelector("input[name='gender']:checked");
-    if (selected) {
-        return selected.value;
-    }
-    return null;
-}
-
-radioButtons.forEach(radioButtons => {
-    radioButtons.addEventListener("click",() => {
-        console.log("Gender: ", findSelected());
-    });
-});
-
-// let profileIcon = document.querySelectorAll("input[name='profile']");
-// let iconSelected = () => {
-//     let icon = document.querySelector("input[name='gender']:checked");
-//     if (icon) {
-//         return icon.value;
-//     }
-//     return null;
-// }
-
-// profileIcon.forEach(profileIcon => {
-//     profileIcon.addEventListener("clilck",() => {
-//         console.log("Profile Icon: ", profileIcon.alt);
-//     });
-// });
-
-const images = document.querySelectorAll('profile');
-images.forEach(profile => {
-    profile.addEventListener('click', () => {
-        console.log(profile);
-    });
 });
