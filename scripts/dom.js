b254374047b1c284e30bf849f9a1f3f96613952c
Manoj Veluru <manojveluru21@gmail.com>
2024-02-08 11:15:40 +0530

    [MANOJ] Added: Added dom features

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,7 +1,54 @@
-alert("Hello");
+// alert("Hello");
 
-function names() {
-    let fname = document.getElementById("fname").value;
+function register_employee() {
+    let name = document.getElementById("name").value;
+    let notes = document.getElementById("notes").value;
 
-    console.log(fname);
+    console.log("Name: ",name);
+    console.log("Notes: ",notes);
+    
+    const obj = {
+        Name: "name",
+        Notes: "notes"
+    }
 }
+document.getElementById("submit").addEventListener("click",function(){
+    register_employee();
+});
+
+let radioButtons = document.querySelectorAll("input[name='gender']");
+let findSelected = () => {
+    let selected = document.querySelector("input[name='gender']:checked");
+    if (selected) {
+        return selected.value;
+    }
+    return null;
+}
+
+radioButtons.forEach(radioButtons => {
+    radioButtons.addEventListener("click",() => {
+        console.log("Gender: ", findSelected());
+    });
+});
+
+// let profileIcon = document.querySelectorAll("input[name='profile']");
+// let iconSelected = () => {
+//     let icon = document.querySelector("input[name='gender']:checked");
+//     if (icon) {
+//         return icon.value;
+//     }
+//     return null;
+// }
+
+// profileIcon.forEach(profileIcon => {
+//     profileIcon.addEventListener("clilck",() => {
+//         console.log("Profile Icon: ", profileIcon.alt);
+//     });
+// });
+
+const images = document.querySelectorAll('profile');
+images.forEach(profile => {
+    profile.addEventListener('click', () => {
+        console.log(profile);
+    });
+});
